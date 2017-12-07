b2e257fccaca06564fc4f24431dea2a6f192537a
Junxiao Shi <git@mail1.yoursunny.com>
2017-12-07 14:52:37 -0700

    dpdk: make ethdev test more reliable

--- integ/dpdk/ethdev/test-ethdev.go
+++ integ/dpdk/ethdev/test-ethdev.go
@@ -6,6 +6,7 @@ import (
 	"github.com/stretchr/testify/require"
 	"ndn-traffic-dpdk/dpdk"
 	"ndn-traffic-dpdk/integ"
+	"time"
 )
 
 func main() {
@@ -41,19 +42,32 @@ func main() {
 	const RX_BURST_SIZE = 6
 	const TX_LOOPS = 100000
 	const TX_BURST_SIZE = 10
+	const MAX_TX_RETRY = 20
+	const TX_RETRY_INTERVAL = 1 * time.Millisecond
+	const RX_FINISH_WAIT = 10 * time.Millisecond
 
 	nReceived := 0
+	rxBurstSizeFreq := make(map[int]int)
+	rxQuit := make(chan int)
 	eal.Slaves[0].RemoteLaunch(func() int {
 		for {
 			pkts := rxq.RxBurst(RX_BURST_SIZE)
-			nReceived = nReceived + len(pkts)
+			rxBurstSizeFreq[len(pkts)]++
 			for _, pkt := range pkts {
-				assert.EqualValues(1, pkt.GetDataLength())
+				nReceived++
+				assert.EqualValuesf(1, pkt.GetDataLength(), "bad RX length at %d", nReceived)
 				pkt.Close()
+			}
+
+			select {
+			case <-rxQuit:
+				return 0
+			default:
 			}
 		}
 	})
 
+	txRetryFreq := make(map[int]int)
 	eal.Slaves[1].RemoteLaunch(func() int {
 		for i := 0; i < TX_LOOPS; i++ {
 			var pkts [TX_BURST_SIZE]dpdk.Mbuf
@@ -64,15 +78,28 @@ func main() {
 				assert.NoError(e)
 				*(*byte)(pktBuf) = byte(j)
 			}
-			res := txq.TxBurst(pkts[:])
-			assert.EqualValuesf(TX_BURST_SIZE, res, "TxBurst incomplete at loop %d", i)
+
+			nSent := 0
+			for nRetries := 0; nRetries < MAX_TX_RETRY; nRetries++ {
+				res := txq.TxBurst(pkts[nSent:])
+				nSent = nSent + int(res)
+				if nSent == TX_BURST_SIZE {
+					txRetryFreq[nRetries]++
+					break
+				}
+				time.Sleep(TX_RETRY_INTERVAL)
+			}
+			assert.EqualValuesf(TX_BURST_SIZE, nSent, "TxBurst incomplete at loop %d", i)
 		}
 		return 0
 	})
 	eal.Slaves[1].Wait()
+	time.Sleep(RX_FINISH_WAIT)
+	rxQuit <- 0
 
-	fmt.Println("nReceived=", nReceived)
 	fmt.Println(port.GetStats())
+	fmt.Println("txtxRetryFreq=", txRetryFreq)
+	fmt.Println("rxBurstSizeFreq=", rxBurstSizeFreq)
 	assert.True(nReceived <= TX_LOOPS*TX_BURST_SIZE)
 	assert.InEpsilon(TX_LOOPS*TX_BURST_SIZE, nReceived, 0.05)
 }
