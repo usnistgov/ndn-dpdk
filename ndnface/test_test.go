389fc2d4188b4852f8b860c043720bd604f4e272
Junxiao Shi <git@mail1.yoursunny.com>
2017-12-22 06:42:33 -0700

    ndn+ndnface: deduplicate packetFromHex function

--- ndnface/test_test.go
+++ ndnface/test_test.go
@@ -3,9 +3,7 @@ package ndnface
 // This file contains test setup procedure and common test helper functions.
 
 import (
-	"encoding/hex"
 	"os"
-	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -26,38 +24,6 @@ func makeAR(t *testing.T) (*assert.Assertions, *require.Assertions) {
 	return assert.New(t), require.New(t)
 }
 
-// Make packet from raw bytes.
-// Memory is allocated from testMp.
-// Caller is responsible for closing the packet.
-func packetFromBytes(input []byte) dpdk.Packet {
-	m, e := dpdktestenv.DirectMp.Alloc()
-	if e != nil {
-		return dpdk.Packet{}
-	}
-
-	pkt := m.AsPacket()
-	seg0 := pkt.GetFirstSegment()
-	e = seg0.AppendOctets(input)
-	if e != nil {
-		panic(e)
-	}
-
-	return pkt
-}
-
-// Make packet from hexadecimal string.
-// The octets must be written as upper case.
-// All characters other than [0-9A-F] are considered as comments and stripped.
 func packetFromHex(input string) dpdk.Packet {
-	s := strings.Map(func(ch rune) rune {
-		if strings.ContainsRune("0123456789ABCDEF", ch) {
-			return ch
-		}
-		return -1
-	}, input)
-	decoded, e := hex.DecodeString(s)
-	if e != nil {
-		return dpdk.Packet{}
-	}
-	return packetFromBytes(decoded)
+	return dpdktestenv.PacketFromHex(input)
 }
