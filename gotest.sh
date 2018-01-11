3ac2f47a3bc0913177a123079a067eb50c1a92cc
Junxiao Shi <git@mail1.yoursunny.com>
2018-01-11 09:40:55 -0700

    build: enable coverage on gotest.sh single package

--- gotest.sh
+++ gotest.sh
@@ -2,7 +2,7 @@
 if [[ $# -eq 0 ]]; then
   find -name '*_test.go' -printf '%h\n' | uniq | xargs -I{} sudo $(which go) test {} -v
 elif [[ $# -eq 1 ]]; then
-  sudo $(which go) test ./$1 -v
+  sudo $(which go) test -cover ./$1 -v
 elif [[ $# -eq 2 ]]; then
   sudo GODEBUG=cgocheck=2 $(which go) test ./$1 -v -run 'Test'$2'.*'
 else
