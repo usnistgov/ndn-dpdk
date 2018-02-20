7c430862a1e443fea5d6aa5cb2b3f0bce4609fa6
Junxiao Shi <git@mail1.yoursunny.com>
2018-02-20 14:37:54 -0700

    ndn: NameComponent tests

--- ndn/name-component_test.go
+++ ndn/name-component_test.go
@@ -1,11 +1,61 @@
 package ndn_test
 
 import (
+	"strings"
 	"testing"
 
 	"ndn-dpdk/dpdk/dpdktestenv"
 	"ndn-dpdk/ndn"
 )
+
+func TestNameComponent(t *testing.T) {
+	assert, _ := makeAR(t)
+
+	const aDigestHex = "DC6D6840C6FAFB773D583CDBF465661C7B4B968E04ACD4D9015B1C4E53E59D6A"
+
+	tests := []struct {
+		input string
+		bad   bool
+		t     uint64
+		v     string
+		str   string
+	}{
+		{input: "FD01", bad: true},         // incomplete TLV-TYPE
+		{input: "08FD01", bad: true},       // incomplete TLV-LENGTH
+		{input: "0802B0", bad: true},       // incomplete TLV-VALUE
+		{input: "0802B0B1B2", bad: true},   // junk at end
+		{input: "0001B0", bad: true},       // TLV-TYPE too small
+		{input: "FE00010000", bad: true},   // TLV-TYPE too large
+		{input: "0104B0B1B2B3", bad: true}, // ImplicitDigest wrong TLV-LENGTH
+
+		{input: "0800", t: 0x08, v: "", str: "..."},
+		{input: "08012E", t: 0x08, v: "2E", str: "...."},
+		{input: "0200", t: 0x02, v: "", str: "2=..."},
+		{input: "FC00", t: 0xFC, v: "", str: "252=..."},
+		{input: "FDFFFF00", t: 0xFFFF, v: "", str: "65535=..."},
+		{input: "96012E", t: 0x96, v: "2E", str: "150=...."},
+		{input: "08052D2E5F7E41", t: 0x08, v: "2D2E5F7E41", str: "-._~A"},
+		{input: "0804002081FF", t: 0x08, v: "002081FF", str: "%00%20%81%FF"},
+		{input: "0120" + aDigestHex, t: 0x01, v: aDigestHex,
+			str: "sha256digest=" + strings.ToLower(aDigestHex)},
+	}
+
+	for _, tt := range tests {
+		comp := ndn.NameComponent(dpdktestenv.BytesFromHex(tt.input))
+		if tt.bad {
+			assert.False(comp.IsValid(), tt.input)
+		} else if assert.True(comp.IsValid(), tt.input) {
+			assert.Equal(ndn.TlvType(tt.t), comp.GetType(), tt.input)
+			assert.Equal(ndn.TlvBytes(dpdktestenv.BytesFromHex(tt.v)), comp.GetValue(), tt.input)
+			assert.Equal(tt.str, comp.String(), tt.input)
+
+			parsed, e := ndn.ParseNameComponent(tt.str)
+			if assert.NoError(e, tt.input) {
+				assert.True(comp.Equal(parsed))
+			}
+		}
+	}
+}
 
 func TestNameComponentFromNumber(t *testing.T) {
 	assert, _ := makeAR(t)
