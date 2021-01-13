56283c99830a2f6b4dfbaa79e3590098875fd167
Junxiao Shi <git@mail1.yoursunny.com>
2021-01-13 18:59:11 +0000

    mk: run 'go mod tidy' as part of code style check

--- mk/format-code.sh
+++ mk/format-code.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 set -eo pipefail
 
-git ls-files 'csrc/**/*.[hc]' | grep -v csrc/vendor | \
-  xargs clang-format-8 -i -style=file
+# C
+git ls-files -- 'csrc/**/*.[hc]' -x ':!:csrc/vendor' | xargs clang-format-8 -i -style=file
 
+# Go
+go mod tidy
 gofmt -l -w -s .
 
+# TypeScript
 node_modules/.bin/xo --fix
 
+# YAML
 git ls-files '*.yml' '*.yaml' | xargs yamllint
 
+# Markdown
 node_modules/.bin/markdownlint --ignore node_modules '**/*.md'
