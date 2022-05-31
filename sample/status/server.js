6d1388ea69e8f0bbc4b04582340853412a692ecc
Junxiao Shi <git@mail1.yoursunny.com>
2022-05-31 15:31:41 +0000

    status: --gqlserver and --listen CLI flags

--- sample/status/server.js
+++ sample/status/server.js
@@ -6,8 +6,32 @@ import Fastify from "fastify";
 import httpProxy from "http-proxy";
 import * as path from "node:path";
 import { fileURLToPath } from "node:url";
+import urlParseLax from "url-parse-lax";
 import webpack from "webpack";
 import devMiddleware from "webpack-dev-middleware";
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
+
+const args = yargs(hideBin(process.argv))
+  .strict()
+  .version(false)
+  .option("gqlserver", {
+    default: "http://127.0.0.1:3030",
+    desc: "NDN-DPDK GraphQL server",
+    type: "string",
+    coerce(input) {
+      return new URL("/", input);
+    },
+  })
+  .option("listen", {
+    default: "127.0.0.1:3333",
+    desc: "Listen host:port",
+    type: "string",
+    coerce(input) {
+      return urlParseLax(input);
+    },
+  })
+  .parseSync();
 
 const publicDir = path.resolve(path.dirname(fileURLToPath(import.meta.url)), "public");
 const compiler = webpack({
@@ -40,7 +64,7 @@ fastify.use(devMiddleware(compiler));
 await fastify.register(FastifyStatic, { root: publicDir });
 
 const proxy = httpProxy.createProxyServer({
-  target: "http://127.0.0.1:3030",
+  target: args.gqlserver.toString(),
   ws: true,
   ignorePath: true,
 });
@@ -49,4 +73,4 @@ fastify.get("/graphql", (request) => {
   proxy.ws(request.raw, request.socket, request.headers);
 });
 
-await fastify.listen(3333, "127.0.0.1");
+await fastify.listen(args.listen.port, args.listen.hostname);
