91821cb59cd80267d2a672511abfd2d2481fd46c
Junxiao Shi <git@mail1.yoursunny.com>
2022-10-07 20:25:06 +0000

    mk: upgrade to TinyGo 0.26.0

--- ndn/ndntestenv/tiny/main.go
+++ ndn/ndntestenv/tiny/main.go
@@ -1,11 +1,17 @@
-// Command tiny ensures part of NDNgo library is compatible with TinyGo compiler.
+// Command tiny verifies that part of NDNgo library is compatible with TinyGo compiler.
 // Supported packages should be written as imports.
 package main
 
 import (
 	_ "github.com/usnistgov/ndn-dpdk/ndn"
 	_ "github.com/usnistgov/ndn-dpdk/ndn/an"
+	_ "github.com/usnistgov/ndn-dpdk/ndn/endpoint"
 	_ "github.com/usnistgov/ndn-dpdk/ndn/keychain"
+	_ "github.com/usnistgov/ndn-dpdk/ndn/l3"
+	_ "github.com/usnistgov/ndn-dpdk/ndn/mgmt"
+	_ "github.com/usnistgov/ndn-dpdk/ndn/rdr"
+	_ "github.com/usnistgov/ndn-dpdk/ndn/rdr/ndn6file"
+	_ "github.com/usnistgov/ndn-dpdk/ndn/segmented"
 	_ "github.com/usnistgov/ndn-dpdk/ndn/tlv"
 )
 
